803f61420b65b7b25299e725b6318f0cf9c47af9
Wildan Belfiore <mikewildan@outlook.com>
2025-07-24 15:20:53 +0700

    update

--- app/Http/Controllers/Api/Web/WebArticleController.php
+++ app/Http/Controllers/Api/Web/WebArticleController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Request as FacadesRequest;
+use Illuminate\Support\Facades\Cache;
 
 class WebArticleController extends Controller
 {
@@ -29,21 +30,25 @@ class WebArticleController extends Controller
 
     public function show($slug)
     {
-        $article = Article::with('category') // Eager load category
-            ->where('slug', $slug)
-            ->where('status', 'publish')
-            ->firstOrFail();
+        // Buat key cache unik berdasarkan slug
+        $cacheKey = "article_detail:{$slug}";
+
+        // Ambil dari cache atau query jika belum ada
+        $article = Cache::remember($cacheKey, now()->addMinutes(60), function () use ($slug) {
+            return Article::with('category')->where('slug', $slug)->where('status', 'publish')->firstOrFail();
+        });
 
         // Ambil IP pengunjung
-        $ip = FacadesRequest::ip();
+        $ip = request()->ip();
 
         // Cek apakah sudah pernah mengunjungi dalam 1 jam terakhir
-        $alreadyVisited = $article->visits()
+        $alreadyVisited = $article
+            ->visits()
             ->where('ip_address', $ip)
             ->where('created_at', '>=', now()->subHour())
             ->exists();
 
-        if (! $alreadyVisited) {
+        if (!$alreadyVisited) {
             $article->visits()->create([
                 'ip_address' => $ip,
             ]);
